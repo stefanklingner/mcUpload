58e276ad5da3956c187869fd8bc0d327497118fd
stefan klingner <stefan_klingner@yahoo.de>
2015-03-16 21:34:52 +0100

    create

---
+++ js/dndUpload.js
@@ -0,0 +1,64 @@
+
+var filelist = []  // Ein Array, das alle hochzuladenden Files enthält
+var totalSize = 0; // Enthält die Gesamtgröße aller hochzuladenden Dateien
+var totalProgress = 0; // Enthält den aktuellen Gesamtfortschritt
+var currentUpload = null; // Enthält die Datei, die aktuell hochgeladen wird
+
+
+function handleDropEvent(event)
+{
+    event.stopPropagation();
+    event.preventDefault();
+
+    // event.dataTransfer.files enthält eine Liste aller gedroppten Dateien
+    for (var i = 0; i < event.dataTransfer.files.length; i++)
+    {
+        filelist.push(event.dataTransfer.files[i]);  // Hinzufügen der Datei zur Uploadqueue
+        totalSize += event.dataTransfer.files[i].size;  // Hinzufügen der Dateigröße zur Gesamtgröße
+    }
+
+    startNextUpload();
+}
+
+function startNextUpload()
+{
+    if (filelist.length)  // Überprüfen, ob noch eine Datei hochzuladen ist
+    {
+        currentUpload = filelist.shift();  // nächste Datei zwischenspeichern
+        uploadFile(currentUpload);  // Upload starten
+    }
+}
+
+function uploadFile(file)
+{
+    var xhr = new XMLHttpRequest();    // den AJAX Request anlegen
+
+    xhr.upload.addEventListener("progress", handleProgress);
+    xhr.addEventListener("load", handleComplete);
+    xhr.addEventListener("error", handleError);
+
+    xhr.open('POST', 'http://localhost:8080/' + 'upload');    // Angeben der URL und des Requesttyps
+
+    var formdata = new FormData();    // Anlegen eines FormData Objekts zum Versenden unserer Datei
+    formdata.append('uploadfile', file);  // Anhängen der Datei an das Objekt
+    xhr.send(formdata);    // Absenden des Requests
+}
+
+function handleComplete(event)
+{
+    totalProgress += currentUpload.size;  // Füge die Größe dem Gesamtfortschritt hinzu
+    startNextUpload(); // Starte den Upload der nächsten Datei
+}
+
+function handleError(event)
+{
+    console.log()    // Die Fehlerbehandlung kann natürlich auch anders aussehen
+    totalProgress += currentUpload.size;  // Die Datei wird dem Progress trotzdem hinzugefügt, damit die Prozentzahl stimmt
+    startNextUpload();  // Starte den Upload der nächsten Datei
+}
+
+function handleProgress(event)
+{
+    var progress = totalProgress + event.loaded;  // Füge den Fortschritt des aktuellen Uploads temporär dem gesamten hinzu
+    document.getElementById('progress').innerHTML = 'Aktueller Fortschritt: ' + (progress / totalSize) * 100 + '%';
+}
---
+++ js/upload.js
@@ -0,0 +1,179 @@
+
+//taken from:
+//http://www.script-tutorials.com/pure-html5-file-upload/
+
+function Upload(elem) {
+
+    var self = this;
+    self.elem = elem;
+    self.counter = 100;
+
+    // common variables
+    var iBytesUploaded = 0;
+    var iBytesTotal = 0;
+    var iPreviousBytesLoaded = 0;
+    var iMaxFilesize = 1048576; // 1MB
+    var oTimer = 0;
+    var sResultFileSize = '';
+
+     secondsToTime = function(secs) { //convert seconds in normal time format
+        var hr = Math.floor(secs / 3600);
+        var min = Math.floor((secs - (hr * 3600)) / 60);
+        var sec = Math.floor(secs - (hr * 3600) - (min * 60));
+
+        if (hr < 10) {
+            hr = "0" + hr;
+        }
+        if (min < 10) {
+            min = "0" + min;
+        }
+        if (sec < 10) {
+            sec = "0" + sec;
+        }
+        if (hr) {
+            hr = "00";
+        }
+        return hr + ':' + min + ':' + sec;
+    };
+
+    bytesToSize = function (bytes) {
+        var sizes = ['Bytes', 'KB', 'MB'];
+        if (bytes == 0) return 'n/a';
+        var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+        return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+    };
+
+    //not used
+    //fileSelected = function () {
+    //
+    //    // get selected file element
+    //    var oFile = this.elem.file;
+    //
+    //    // filter for image files
+    //    var rFilter = /^(image\/bmp|image\/gif|image\/jpeg|image\/png|image\/tiff)$/i;
+    //    if (!rFilter.test(oFile.type)) {
+    //        document.getElementById('error').style.display = 'block';
+    //        return;
+    //    }
+    //
+    //    // little test for filesize
+    //    if (oFile.size > iMaxFilesize) {
+    //        this.elem.warnsize = true;
+    //        return;
+    //    }
+    //
+    //    // get preview element
+    //    var oImage = document.getElementById('preview');
+    //
+    //    // prepare HTML5 FileReader
+    //    var oReader = new FileReader();
+    //    oReader.onload = function (e) {
+    //
+    //        // e.target.result contains the DataURL which we will use as a source of the image
+    //        oImage.src = e.target.result;
+    //
+    //        oImage.onload = function () { // binding onload event
+    //
+    //            // we are going to display some custom image information here
+    //            sResultFileSize = bytesToSize(oFile.size);
+    //            //this.elem.fileinfo = 'block';
+    //            this.elem.filename = 'Name: ' + oFile.name;
+    //            this.elem.filesize = 'Size: ' + sResultFileSize;
+    //            this.elem.filetype = 'Type: ' + oFile.type;
+    //            this.elem.filedim = 'Dimension: ' + oImage.naturalWidth + ' x ' + oImage.naturalHeight;
+    //
+    //        };
+    //    };
+    //
+    //    // read selected file as DataURL
+    //    oReader.readAsDataURL(oFile);
+    //}
+
+    this.startUploading = function () {
+
+        var uploadElem = self.elem.myForm;
+        if (uploadElem == undefined) {
+            return;
+        }
+
+        // cleanup all temp states
+        iPreviousBytesLoaded = 0;
+
+        // get form data for POSTing
+        var vFD = new FormData(uploadElem);
+
+        // create XMLHttpRequest object, adding few event listeners, and POSTing our data
+        var oXHR = new XMLHttpRequest();
+        oXHR.upload.addEventListener('progress', uploadProgress, false);
+        oXHR.addEventListener('load', uploadFinish, false);
+        oXHR.addEventListener('error', uploadError, false);
+        oXHR.addEventListener('abort', uploadAbort, false);
+        oXHR.open('POST', document.URL + 'upload');
+        oXHR.send(vFD);
+
+        // set inner timer
+        oTimer = setInterval(doInnerUpdates, 300);
+    }
+
+    doInnerUpdates = function () { // display the upload speed
+        var iCB = iBytesUploaded;
+        var iDiff = iCB - iPreviousBytesLoaded;
+
+        // if nothing new loaded - exit
+        if (iDiff == 0)
+            return;
+
+        iPreviousBytesLoaded = iCB;
+        iDiff = iDiff * 2;
+        var iBytesRem = iBytesTotal - iPreviousBytesLoaded;
+        var secondsRemaining = iBytesRem / iDiff;
+
+        // update speed info
+        var iSpeed = iDiff.toString() + 'B/s';
+        if (iDiff > 1024 * 1024) {
+            iSpeed = (Math.round(iDiff * 100 / (1024 * 1024)) / 100).toString() + 'MB/s';
+        } else if (iDiff > 1024) {
+            iSpeed = (Math.round(iDiff * 100 / 1024) / 100).toString() + 'KB/s';
+        }
+
+        self.elem.speed(iSpeed);
+        self.elem.remaining(secondsToTime(secondsRemaining));
+    }
+
+    uploadProgress = function (e) { // upload process in progress
+        if (e.lengthComputable) {
+            iBytesUploaded = e.loaded;
+            iBytesTotal = e.total;
+            var iPercentComplete = Math.round(e.loaded * 100 / e.total);
+            var iBytesTransfered = bytesToSize(iBytesUploaded);
+
+            self.elem.processed(iPercentComplete); // * 4).toString() + 'px';
+            self.elem.b_transfered = iBytesTransfered;
+            if (iPercentComplete == 100) {
+
+
+            }
+        } else {
+            console.log("unable to compute");
+        }
+    }
+
+    uploadFinish = function (e) { // upload successfully finished
+
+        console.log("counter " + self.counter);
+        self.elem.processed(100);
+
+        clearInterval(oTimer);
+    }
+
+    uploadError = function (e) { // upload error
+        console.log("upload error");
+        clearInterval(oTimer);
+    }
+
+    uploadAbort = function (e) { // upload abort
+        console.log("upload abort");
+        clearInterval(oTimer);
+    }
+
+}
